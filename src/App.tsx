a6d4dec63e0c099b300fb6706fc7c27bb8acd0f1
Eduardo Web Solutions <eduardowebsolutions@gmail.com>
2023-08-07 21:24:22 -0300

    feat: keys and other divs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">My Drum Machine</h1>
+      <div id="Drum-machine">
+        <div id="keys">
+          <div>q</div>
+          <div>w</div>
+          <div>e</div>
+          <div>a</div>
+          <div>s</div>
+          <div>d</div>
+          <div>z</div>
+          <div>x</div>
+          <div>c</div>
+        </div>
+        <div>Logo</div>
+        <div>Power</div>
+        <div>Drum Title</div>
+        <div>Volume</div>
+        <div>Bank Nodle</div>
+      </div>
     </>
   );
 }
